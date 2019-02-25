1eb4c080a91c262b7f24e6f58a2566142a23649c
Robert Flanagan <robertflanagan95@gmail.com>
2019-02-25 00:52:37 -0500

    basic bootstrap layout

--- public/app.js
+++ public/app.js
@@ -1,128 +1,40 @@
-// We'll be rewriting the table's data frequently, so let's make our code more DRY
-// by writing a function that takes in 'animals' (JSON) and creates a table body
 function displayResults(articles) {
-  // First, empty the table
   $("#articles").empty();
 
-  // Then, for each entry of that json...
   articles.forEach(function(article) {
-    // Append each of the animal's properties to the table
-    var newArticle = $("<cardbody>").append(
-      $("<h2>").text(article.headline),
-      $("<h5>").text(article.author),
-      $("<p>").text(article.description),
-      // $("<td>").text(animal.weight),
-      // $("<td>").text(animal.whatIWouldReallyCallIt)
+    var headline = $("<h2>");
+    headline.text(article.headline);
+    headline.addClass("card-title");
+
+    var author = $("<h5>");
+    if (article.author) {
+      author.text(article.author);
+    }
+    var description = $("<p>");
+    description.text(article.description);
+    description.addClass("card-text");
+
+    var articleImg = $("<img>");
+    articleImg.attr("src", article.imageURL);
+    articleImg.attr("style", "width:165px;height:110px");
+
+    var cardContents = $("<div>").append(
+      headline,
+      author,
+      description,
+      articleImg
     );
+
+    cardContents.addClass("card-body");
+
+    var newArticle = $("<div>");
+    newArticle.append(cardContents);
+    newArticle.addClass("card");
 
     $("#articles").append(newArticle);
   });
 }
 
-// // Bonus function to change "active" header
-// function setActive(selector) {
-//   // remove and apply 'active' class to distinguish which column we sorted by
-//   $("th").removeClass("active");
-//   $(selector).addClass("active");
-// }
-
-// 1: On Load
-// ==========
-
-// First thing: ask the back end for json with all animals
 $.getJSON("/articles", function(data) {
-  // Call our function to generate a table body
   displayResults(data);
-});
-
-// // 2: Button Interactions
-// // ======================
-
-// // When user clicks the weight sort button, display table sorted by weight
-// $("#weight-sort").on("click", function() {
-//   // Set new column as currently-sorted (active)
-//   setActive("#animal-weight");
-
-//   // Do an api call to the back end for json with all animals sorted by weight
-//   $.getJSON("/weight", function(data) {
-//     // Call our function to generate a table body
-//     displayResults(data);
-//   });
-// });
-
-// // When user clicks the name sort button, display the table sorted by name
-// $("#name-sort").on("click", function() {
-//   // Set new column as currently-sorted (active)
-//   setActive("#animal-name");
-
-//   // Do an api call to the back end for json with all animals sorted by name
-//   $.getJSON("/name", function(data) {
-//     // Call our function to generate a table body
-//     displayResults(data);
-//   });
-// });
-
-
-
-// Whenever someone clicks a p tag
-$(document).on("click", "p", function() {
-  // Empty the notes from the note section
-  $("#notes").empty();
-  // Save the id from the p tag
-  var thisId = $(this).attr("data-id");
-
-  // Now make an ajax call for the Article
-  $.ajax({
-    method: "GET",
-    url: "/articles/" + thisId
-  })
-    // With that done, add the note information to the page
-    .then(function(data) {
-      console.log(data);
-      // The title of the article
-      $("#notes").append("<h2>" + data.title + "</h2>");
-      // An input to enter a new title
-      $("#notes").append("<input id='titleinput' name='title' >");
-      // A textarea to add a new note body
-      $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
-      // A button to submit a new note, with the id of the article saved to it
-      $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
-
-      // If there's a note in the article
-      if (data.note) {
-        // Place the title of the note in the title input
-        $("#titleinput").val(data.note.title);
-        // Place the body of the note in the body textarea
-        $("#bodyinput").val(data.note.body);
-      }
-    });
-});
-
-// When you click the savenote button
-$(document).on("click", "#savenote", function() {
-  // Grab the id associated with the article from the submit button
-  var thisId = $(this).attr("data-id");
-
-  // Run a POST request to change the note, using what's entered in the inputs
-  $.ajax({
-    method: "POST",
-    url: "/articles/" + thisId,
-    data: {
-      // Value taken from title input
-      title: $("#titleinput").val(),
-      // Value taken from note textarea
-      body: $("#bodyinput").val()
-    }
-  })
-    // With that done
-    .then(function(data) {
-      // Log the response
-      console.log(data);
-      // Empty the notes section
-      $("#notes").empty();
-    });
-
-  // Also, remove the values entered in the input and textarea for note entry
-  $("#titleinput").val("");
-  $("#bodyinput").val("");
 });
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var exphbs = require("express-handlebars");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 
@@ -37,17 +38,12 @@ app.get("/scrape", function(req, res) {
       // Save an empty result object
       let result = {};
 
-      console.log("=================================");
-
       result.headline = $(element).find("h2").text();
       result.link = "https://nytimes.com/section/politics"+ $(element).find("a").attr("href");
       result.imageURL = $(element).find(".toneNews").attr("itemid");
       const descSplit = $(element).find("p").text().split("By ");
       result.description = descSplit[0];
       result.author = descSplit[1];
-
-      console.log(result);
-      console.log("=================================");
 
 
       // Create a new Article using the `result` object built from scraping
@@ -94,25 +90,6 @@ app.get("/articles/:id", function(req, res) {
     .catch(function(err) {
       res.json(err);
     });
-});
-
-// Route for saving/updating an Article's associated Note
-app.post("/articles/:id", function(req, res) {
-  // TODO
-  // ====
-  // save the new note that gets posted to the Notes collection
-  // then find an article from the req.params.id
-  // and update it's "note" property with the _id of the new note
-  db.Note.create(req.body)
-  .then(function(data) {
-    return db.Article.findOneAndUpdate({ _id: req.params.id }, {note: data._id }, { new: true});
-  })
-  .then(function(response) {
-    res.json(response);
-  })
-  .catch(function(err) {
-    res.json(err);
-  });
 });
 
 // Start the server
